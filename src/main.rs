e395c1506e4e65bba6e986cfae7c6547b1938807
alexbostock <alex@alexbostock.co.uk>
2019-08-14 22:27:58 +0100

    Define puzzle data structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod sudoku {
+    enum PuzzleSize {
+        Four,
+        Six,
+        Nine,
+        Twelve,
+        Sixteen,
+        Twenty,
+        TwentyFive,
+    }
+
+    enum Cell {
+        Value(i8),
+        PossibleValues(std::collections::HashSet<i8>),
+    }
+
+    struct Puzzle {
+        size: PuzzleSize,
+        grid: Vec<Vec<Cell>>,
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
