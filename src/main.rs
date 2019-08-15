8664aa1c63d0151ffd1b510fb30f7ac81fe6a823
alexbostock <alex@alexbostock.co.uk>
2019-08-15 15:53:59 +0100

    Implement brute force solver

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod sudoku {
+    #[derive(Clone)]
     enum PuzzleSize {
         Four,
         Six,
@@ -9,14 +10,53 @@ mod sudoku {
         TwentyFive,
     }
 
+    #[derive(Clone)]
     enum Cell {
         Value(i8),
         PossibleValues(std::collections::HashSet<i8>),
     }
 
-    struct Puzzle {
+    #[derive(Clone)]
+    pub struct Puzzle {
         size: PuzzleSize,
         grid: Vec<Vec<Cell>>,
+    }
+
+    pub fn brute_force(p: Puzzle) -> Option<Puzzle> {
+        let r = Puzzle {
+            size: p.size.clone(),
+            grid: Vec::new(),
+        };
+
+        return try_combinations(&p, &r);
+    }
+
+    fn try_combinations(p: &Puzzle, r: &Puzzle) -> Option<Puzzle> {
+        let mut s = r.clone();
+
+        for (i, row) in p.grid.iter().enumerate() {
+            s.grid.push(Vec::new());
+            for (j, cell) in row.iter().enumerate() {
+                match cell {
+                    Cell::Value(v) => s.grid[i].push(Cell::Value(*v)),
+                    Cell::PossibleValues(vs) => {
+                        s.grid[i].push(Cell::Value(0));
+
+                        for (num, v) in vs.iter().enumerate() {
+                            s.grid[i][j] = Cell::Value(*v);
+                            if let Some(puzzle) = try_combinations(p, &s) {
+                                s = puzzle;
+                                break;
+                            } else if num == vs.len() - 1 {
+                                return None;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return Some(s);
     }
 }
 
